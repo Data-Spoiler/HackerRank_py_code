fd031e8aa2eca3e45fee0ec5c36c1055fa8c8147
ArunRathi <arunrathi201@gmail.com>
2020-09-05 12:25:22 +0530

    Update Codes.py

--- Codes.py
+++ Codes.py
@@ -1,5 +1,5 @@
-collections.Counter()
-A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
+#collections.Counter()
+#A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
 
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
@@ -15,3 +15,17 @@ for i in range(N):
             shoe[pair[0]] -= 1
             sum += pair[1]
 print(sum)
+
+# itertools.permutations(iterable[, r])
+# This tool returns successive r length permutations of elements in an iterable. 
+# If  r is not specified or is None, then r defaults to the length of the iterable, and all possible full length permutations are generated.
+from itertools import permutations
+s,n = input().split()
+print(*[''.join(i) for i in permutations(sorted(s),int(n))],sep='\n')
+
+# Print function
+# The included code stub will read an integer, N , from STDIN.
+# Without using any string methods, try to print the following: 1234...N
+if __name__ == '__main__':
+    n = int(input())
+    print(*range(1, n+1), sep='')
